3aeacb9a08f751ba36675046833d0b682206da32
moon-hai <moon.hai@1pac.vn>
2019-01-01 18:56:24 +0700

    split message to component

---
+++ src/components/Message.vue
@@ -0,0 +1,11 @@
+<template>
+  <p class="tag is-success" v-if="this.display">{{ this.content }}</p>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  props: ['display', 'content']
+}
+</script>
+
---
+++ src/mixins/message.js
@@ -0,0 +1,7 @@
+import Message from '../components/Message'
+
+export default {
+  components: {
+    Message
+  }
+}
